432c548f991c15bbe680bd4f9a93687d83648eb6
MartaBahrynivska <martabagrynivska@googlemail.com>
2023-10-19 17:54:39 -0400

    done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const colorName = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const body = document.body;
+
+
+const changeColor = () => {
+  colorName.textContent = body.style.backgroundColor = getRandomHexColor();
+};
+
+
+changeColorBtn.addEventListener('click', changeColor);

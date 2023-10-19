e01f17891a3a13473d658b18bd79329cb5e8c289
MartaBahrynivska <martabagrynivska@googlemail.com>
2023-10-19 17:37:46 -0400

    aftertask8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const submitForm = document.querySelector('.login-form');
+submitForm.addEventListener('submit', handlerSubmit);
 
+function handlerSubmit(evt)  {
+    evt.preventDefault();
+    const { email, password } = evt.currentTarget.elements;
+
+    if (!email.value.trim() || !password.value.trim()) {
+        return alert('Please fill in all the required fields!');
+    } else {
+        const data = {
+            email: email.value,
+            password: password.value,
+        }
+
+        console.log(data);
+        evt.currentTarget.reset();
+    }
+}
